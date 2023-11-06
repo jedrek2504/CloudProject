7c80a4136af8196cd0d0e76730810cfb19d0d85a
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2023-11-06 19:24:41 +0100

    Initial config of the project

---
+++ cloud_project/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "cloudprojectapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "cloudFilesHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
