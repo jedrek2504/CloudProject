6bef2716e31dd7ebf42b45e5bb6654cf0895785e
Jedrzej Kostyk <jedrek2504@gmail.com>
2023-11-06 19:37:05 +0100

    Linking frontend WIP

--- cloud_project/src/App.js
+++ cloud_project/src/App.js
@@ -1,52 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import Amplify, { API } from 'aws-amplify'
-import React, { useEffect, useState } from 'react'
+// App.js
 
-const myAPI = "cloudprojectapi"
-const path = '/items'; 
+import React, { useState } from 'react';
+import Amplify, { API } from 'aws-amplify';
+import './App.css';
+
+Amplify.configure({
+    // Your Amplify configuration goes here
+});
+
+const myAPI = "cloudprojectapi";
+const path = '/items'; // The path set up in your REST API
 
 const App = () => {
-  const [input, setInput] = useState("")
-  const [customers, setCustomers] = useState([])
+    const [message, setMessage] = useState("");
 
-  //Function to fetch from our backend and update customers array
-  function getCustomer(e) {
-    let customerId = e.input
-    API.get(myAPI, path)
-       .then(response => {
-         console.log(response)
-         let newCustomers = [...customers]
-         newCustomers.push(response)
-         setCustomers(newCustomers)
+    // Function to fetch a message from our backend
+    const fetchMessage = () => {
+        API.get(myAPI, path)
+            .then(response => {
+                console.log(response);
+                setMessage(response); // Assuming the response is just a simple string
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
 
-       })
-       .catch(error => {
-         console.log(error)
-       })
-  }
-
-  return (
-    
-    <div className="App">
-      <h1>Super Simple React App</h1>
-      <div>
-          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
-      </div>
-      <br/>
-      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
-
-      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
-      {
-       customers.map((thisCustomer, index) => {
-         return (
-        <div key={thisCustomer.customerId}>
-          <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
-        </div>)
-       })
-      }
-    </div>
-  )
-}
+    return (
+        <div className="App">
+            <h1>Connect Front to Back</h1>
+            <button onClick={fetchMessage}>Fetch Message</button>
+            {message && (
+                <p>Response from backend: {message}</p>
+            )}
+        </div>
+    );
+};
 
 export default App;
