3a4d596767b626e57d3dd972a5640b550614d97e
Jędrzej Kostyk <jedrek2504@gmail.com>
2023-11-10 15:44:36 +0100

    Getting items from S3 bucket (#2)
    * s3 bucket wip
    
    * adding s3 generated config files
    
    * Getting objects from S3 bucket using lambda function works well.
    
    * Got rid of unnecessary json files

--- cloud_project/src/App.js
+++ cloud_project/src/App.js
@@ -1,40 +1,47 @@
-// App.js
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
 import './App.css';
-import {Authenticator} from "@aws-amplify/ui-react";
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-
-const myAPI = "cloudprojectapi";
-const path = '/items'; // The path set up in your REST API
+const myAPI = "cloudprojectapi"; // This should match your Amplify API name
+const path = '/items'; // The path you configured in your API Gateway
 
 const App = () => {
-    const [message, setMessage] = useState("");
+    const [items, setItems] = useState([]);
 
-    // Function to fetch a message from our backend
-    const fetchMessage = () => {
-        API.get(myAPI, path)
-            .then(response => {
-                console.log(response);
-                setMessage(response); // Assuming the response is just a simple string
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
+    useEffect(() => {
+        // Function to fetch items from our backend
+        const fetchItems = async () => {
+            try {
+                // API.get automatically parses the JSON response for you
+                const apiResponse = await API.get(myAPI, path);
+                setItems(apiResponse);
+            } catch (error) {
+                console.error('Error fetching items from S3', error);
+            }
+        };
+
+        fetchItems(); // Fetch items when the component mounts
+    }, []);
 
     return (
         <Authenticator>
-            {({signOut}) => (
+            {({ signOut, user }) => (
                 <div className="App">
-                    <h1>Connect Front to Back</h1>
-                    <button onClick={fetchMessage}>Fetch Message</button>
-                    {message && (
-                        <p>Response from backend: {message}</p>
+                    <h1>Cloud Project S3 Bucket Contents</h1>
+                    {items.length > 0 ? (
+                        <ul>
+                            {items.map((item, index) => (
+                                <li key={index}>
+                                    Key: {item.key}, Size: {item.size}
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>No items in S3 bucket to display.</p>
                     )}
-                    <button onClick={signOut}>Sign Out</button>`
+                    {user && <button onClick={signOut}>Sign Out</button>}
                 </div>
             )}
         </Authenticator>
