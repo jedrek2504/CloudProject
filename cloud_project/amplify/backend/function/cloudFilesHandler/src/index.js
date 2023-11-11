009b183390f0d78cb91720c88c57b4bc984d9bab
Jedrzej Kostyk <jedrek2504@gmail.com>
2023-11-11 20:32:21 +0100

    Got rid of unnecessary log statements in lambda function

--- cloud_project/amplify/backend/function/cloudFilesHandler/src/index.js
+++ cloud_project/amplify/backend/function/cloudFilesHandler/src/index.js
@@ -12,9 +12,7 @@ const myBucketName = 'cloudprojects3bucket183954-dev';
  */
 exports.handler = async (event) => {
     try {
-        console.log("Received event:", JSON.stringify(event, null, 2));
         const cognitoIdentityId = event.headers['X-Identity-Id'] || event.headers['x-identity-id']; // Headers are case-insensitive
-        console.log("Cognito Identity ID in Lambda:", cognitoIdentityId);
         // Fetch the list of items from S3
         const s3Response = await s3.listObjectsV2({
             Bucket: myBucketName,
