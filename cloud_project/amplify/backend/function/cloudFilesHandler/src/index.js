25c05fbe628b8c28efa987e33ab9529078e2a093
Jedrzej Kostyk <jedrek2504@gmail.com>
2023-11-11 21:08:32 +0100

    Added correct lambda function - for real this time

--- cloud_project/amplify/backend/function/cloudFilesHandler/src/index.js
+++ cloud_project/amplify/backend/function/cloudFilesHandler/src/index.js
@@ -1,4 +1,3 @@
-// Import the AWS SDK
 const AWS = require('aws-sdk');
 
 // Initialize the S3 service object
@@ -7,45 +6,45 @@ const s3 = new AWS.S3();
 // The name of the bucket
 const myBucketName = 'cloudprojects3bucket183954-dev';
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
 exports.handler = async (event) => {
     try {
-        const cognitoIdentityId = event.headers['X-Identity-Id'] || event.headers['x-identity-id']; // Headers are case-insensitive
+        const cognitoIdentityId = event.headers['X-Identity-Id'] || event.headers['x-identity-id'];
+
         // Fetch the list of items from S3
-        const s3Response = await s3.listObjectsV2({
-            Bucket: myBucketName,
-        }).promise();
+        const s3Response = await s3.listObjectsV2({ Bucket: myBucketName }).promise();
+        const itemsWithMetadata = [];
 
-        // Extract the file information from the S3 response
-        const items = s3Response.Contents
-        .filter(file => file.TagCount > 0 && file.TagSet.find(tag => tag.Key === 'Owner' && tag.Value === cognitoIdentityId))
-        .map(file => {
-            // Remove the 'public/' prefix from the key
-            const keyWithoutPrefix = file.Key.replace(/^public\//, '');
-            return { key: keyWithoutPrefix, size: file.Size };
-        });
+        for (const object of s3Response.Contents) {
+            const headResponse = await s3.headObject({ Bucket: myBucketName, Key: object.Key }).promise();
+            const metadataIdentityId = headResponse.Metadata['owner'];
 
-        // Return the file list as the response
+            if (metadataIdentityId === cognitoIdentityId) {
+                itemsWithMetadata.push({
+                    key: object.Key.replace(/^public\//, ''),
+                    size: object.Size
+                });
+            }
+        }
+
         return {
             statusCode: 200,
             headers: {
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*"
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Identity-Id",
+                "Access-Control-Allow-Methods": "GET,POST,OPTIONS,PUT,DELETE"
             },
-            body: JSON.stringify(items),
+            body: JSON.stringify(itemsWithMetadata),
         };
-
     } catch (error) {
-        console.log(error);
+        console.error('Error fetching items from S3', error);
         return {
             statusCode: 500,
             headers: {
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*"
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Identity-Id",
+                "Access-Control-Allow-Methods": "GET,POST,OPTIONS,PUT,DELETE"
             },
-            body: JSON.stringify('Error fetching from S3'+ error.message),
+            body: JSON.stringify({ message: 'Error fetching from S3', details: error.message }),
         };
     }
 };
