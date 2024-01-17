39bda55658aff33c3b731706617808bf3fe27191
Jedrzej Kostyk <jedrek2504@gmail.com>
2024-01-18 00:02:42 +0100

    updated lambda function

--- cloud_project/amplify/backend/function/cloudFilesHandler/src/index.js
+++ cloud_project/amplify/backend/function/cloudFilesHandler/src/index.js
@@ -4,7 +4,7 @@ const myBucketName = 'cloudprojects3bucket183954-dev';
 
 exports.handler = async (event) => {
     console.log("Received event:", JSON.stringify(event, null, 2));
-    const cognitoIdentityId = event.requestContext.authorizer.claims.sub; // Use the sub claim as the identity id
+    const cognitoIdentityId = event.requestContext.authorizer.claims.sub;
 
     try {
         console.log("Fetching list of object versions from S3");
@@ -15,17 +15,17 @@ exports.handler = async (event) => {
 
         console.log("S3 listObjectVersions response:", JSON.stringify(s3Response, null, 2));
 
-        const latestVersions = s3Response.Versions.filter(version => version.IsLatest);
-        console.log("Filtered latest versions:", JSON.stringify(latestVersions, null, 2));
-
-        const items = latestVersions.map(version => {
-            const keyWithoutPrefix = version.Key.replace(/^public\//, '');
-            return {
-                key: keyWithoutPrefix,
-                size: version.Size,
-                versionId: version.VersionId
-            };
-        });
+        const items = s3Response.Versions
+            .filter(version => !version.IsDeleteMarker)
+            .map(version => {
+                const keyWithoutPrefix = version.Key.replace(/^public\//, '');
+                return {
+                    key: keyWithoutPrefix,
+                    size: version.Size,
+                    versionId: version.VersionId,
+                    lastModified: version.LastModified
+                };
+            });
 
         console.log("Mapped items to return:", JSON.stringify(items, null, 2));
         return {
@@ -36,7 +36,6 @@ exports.handler = async (event) => {
             },
             body: JSON.stringify(items),
         };
-
     } catch (error) {
         console.error("Error fetching from S3:", error);
         return {
