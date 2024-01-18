6eafb959e67ecb3f7138c84719838da64fcc0443
Jedrzej Kostyk <jedrek2504@gmail.com>
2024-01-18 13:59:46 +0100

    Displaying Last Modified

--- cloud_project/src/view/HomePage.js
+++ cloud_project/src/view/HomePage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Flex, Text, View, Button } from '@aws-amplify/ui-react';
+import {Flex, Text, View, Button} from '@aws-amplify/ui-react';
 import Navbar from './NavBar';
 
-const HomePage = ({ items, onFileChange, handleUpload, downloadFile, deleteFile, signOut, downloadedImageUrl }) => (
+const HomePage = ({items, onFileChange, handleUpload, downloadFile, deleteFile, signOut, downloadedImageUrl}) => (
     <View className="App">
-        <Navbar signOut={signOut} />
+        <Navbar signOut={signOut}/>
         <Flex direction="column" alignItems="center" justifyContent="center">
             <Text variant="h1">Cloud Project S3 Bucket Contents</Text>
             <input type="file" multiple onChange={onFileChange}/>
@@ -13,7 +13,8 @@ const HomePage = ({ items, onFileChange, handleUpload, downloadFile, deleteFile,
                 <ul>
                     {items.map((item, index) => (
                         <li key={index}>
-                            Key: {item.key}, Size: {item.size}
+                            Key: {item.key}, Size: {item.size}, Last
+                            Modified: {new Date(item.lastModified).toLocaleString()}
                             <Button onClick={() => downloadFile(item.key, item.versionId)}>Download</Button>
                             <Button onClick={() => deleteFile(item.key, item.versionId)}>Delete</Button>
                         </li>
@@ -22,7 +23,7 @@ const HomePage = ({ items, onFileChange, handleUpload, downloadFile, deleteFile,
             ) : (
                 <Text>No items in S3 bucket to display.</Text>
             )}
-            {downloadedImageUrl && <img src={downloadedImageUrl} alt="Downloaded Image" />}
+            {downloadedImageUrl && <img src={downloadedImageUrl} alt="Downloaded Image"/>}
             <Button onClick={signOut}>Sign Out</Button>
         </Flex>
     </View>
